284e5ebc69d26c24cf3d8864d6ffb04e6958317c
Yashwanth-InOrg <yashwanth.p.s@inorg.com>
2024-07-08 13:24:24 +0530

    Feature: Polymorphism and its Types

---
+++ src/Day1/Polymorphism/MethodOverriding/GFG.java
@@ -0,0 +1,19 @@
+package Day1.Polymorphism.MethodOverriding;
+
+// Class 4 - Main class
+class GFG {
+    // Main driver method
+    public static void main(String[] args) {
+        // Creating object of class 1
+        Parent a; // Defining/Declaring
+        // Now we will be calling print methods inside main() method
+        a = new subclass1(); // Initializing
+        a.Print();
+        a = new subclass2(); // Upcasting
+        a.Print();
+    }
+}
+
+// Upcasting - Reference is of Parent Type and the Object is of Child Type
+// Down casting - Reference is of Child Type and the Object is of Parent Type
+// TODO: Example of Down casting and Understand why is it not Recommended
---
+++ src/Day1/Polymorphism/MethodOverriding/subclass2.java
@@ -0,0 +1,11 @@
+package Day1.Polymorphism.MethodOverriding;
+
+// Class 3
+// Helper class
+class subclass2 extends Parent {
+    // Method
+    void Print() {
+        // Print statement
+        System.out.println("subclass2");
+    }
+}
