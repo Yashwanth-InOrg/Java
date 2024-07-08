0b6e24e56731abdc9ffbc4b2c5797631b2d9c00f
KajalTrehan-inorg <kajal.p.trehan@inorg.com>
2024-07-08 20:42:28 +0530

    Feature: Single inheritance example

---
+++ src/Day1/SingleInheritance.java
@@ -0,0 +1,16 @@
+package Day1;
+
+
+    class Animal{
+        void eat(){System.out.println("eating...");}
+    }
+    class Dog extends Animal{
+        void bark(){System.out.println("barking...");}
+    }
+    public class SingleInheritance {
+        public static void main(String args[]){
+            Dog d=new Dog();
+            d.bark();
+            d.eat();
+        }}
+
