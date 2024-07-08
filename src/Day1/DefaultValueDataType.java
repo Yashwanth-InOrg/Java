0921c08720ba7e5e4aea5e02d073fd78ac2749ef
KajalTrehan-inorg <kajal.p.trehan@inorg.com>
2024-07-08 15:47:08 +0530

    Feature: Checking the default values for primitive data types

---
+++ src/Day1/DefaultValueDataType.java
@@ -0,0 +1,21 @@
+package Day1;
+
+public class DefaultValueDataType {
+
+        static byte a;
+        static short b;
+        static char c;
+        static float d;
+        static double e;
+        static boolean f;
+
+        public static void main(String[] args) {
+
+            System.out.println("Default value of byte Type = " +a);
+            System.out.println("Default value of short Type = " +b);
+            System.out.println("Default value of char Type = " +c);
+            System.out.println("Default value of float type = " +d);
+            System.out.println("Default value of double Type = " +e);
+            System.out.println("Default value of boolean Type = " +f);
+        }
+}
