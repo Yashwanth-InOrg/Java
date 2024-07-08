d1929ab278a0a0ca2d09683020e652309b21a47b
abhinavshet-97 <abhinav.shet@vivacommerce.com>
2024-07-08 20:49:59 +0530

    Feature: Hierarchial Inheritance

---
+++ src/HierarchicalI.java
@@ -0,0 +1,16 @@
+class Animal2{
+    void eat(){System.out.println("eating...");}
+}
+class Dog2 extends Animal2{
+    void bark(){System.out.println("barking...");}
+}
+class Cat extends Animal2{
+    void meow(){System.out.println("meowing...");}
+}
+public class HierarchicalI {
+    public static void main(String args[]){
+        Cat c=new Cat();
+        c.meow();
+        c.eat();
+//c.bark();//C.T.Error
+    }}
