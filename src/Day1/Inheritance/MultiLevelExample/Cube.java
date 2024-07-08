6259387f739619a45d788b0b7bcd61453a840d37
Yashwanth-InOrg <yashwanth.p.s@inorg.com>
2024-07-08 13:24:04 +0530

    Feature: Inheritance and its Types

---
+++ src/Day1/Inheritance/MultiLevelExample/Cube.java
@@ -0,0 +1,7 @@
+package Day1.Inheritance.MultiLevelExample;
+
+class Cube extends Rectangle { // Grand Child Class
+   public void volume() {
+      System.out.println("Inside volume");
+   }
+}
---
+++ src/Day1/Inheritance/SingleInheritance/Test.java
@@ -0,0 +1,15 @@
+package Day1.Inheritance.SingleInheritance;
+
+// driver class
+public class Test { // TODO: Examples Hierarchical, Multi-Level, Multiple Inheritance
+    public static void main(String args[]) {
+        MountainBike mb = new MountainBike(3, 100, 25);
+        System.out.println(mb.toString());
+    }
+}
+
+/* Types of Inheritance
+- Single Inheritance
+- Hierarchical Inheritance
+- Multi-Level Inheritance
+- Multiple Inheritance - Not Possible (Diamond Problem) */
