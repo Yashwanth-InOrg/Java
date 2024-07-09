868c2021612b0d06fdd681ada405feb07db6da0e
Yashwanth-InOrg <yashwanth.p.s@inorg.com>
2024-07-09 13:34:18 +0530

    Feature: Static vs Final Examples

---
+++ src/Day2/StaticVsFinal/Parent.java
@@ -0,0 +1,13 @@
+package Day2.StaticVsFinal;
+
+// Java Program to illustrate Final keyword
+public class Parent { // Class 1 --> Super-class
+    public static void firstName() { // Method 1 - To declare first name
+        System.out.println("First Name"); // Passing name and print it
+    }
+
+    public void surName() { // Method 2 - To declare last name
+        System.out.println("Last Name"); // Passing name and print it
+    }
+}
+ 
